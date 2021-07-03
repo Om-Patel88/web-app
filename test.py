f9e4f4873fade1d4240aa070a8cc66778aeaa0d5
Om-Patel88 <68469640+Om-Patel88@users.noreply.github.com>
2021-07-03 18:17:25 +0530

    Just to check web-app

---
+++ test.py
@@ -0,0 +1,5 @@
+import streamlit as st
+num = st.text_input('Enter Text')
+#st.write('cube of ', num, 'is', num**3)
+#st.sidebar.title('This is the app to calculate the cube root of a number')
+st.write(num)
